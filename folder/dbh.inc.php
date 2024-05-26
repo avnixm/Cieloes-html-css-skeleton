c145658ddec964f30b0bb31e0c73901cb11766fd
BEnJIEBALNEG <164105755+BEnJIEBALNEG@users.noreply.github.com>
2024-05-26 12:08:21 +0800

    adding php file
    yung nagamit kolang dito para ma change yung username and email  ay yung userupdate.php, tapos yung dbh.php yung connection sa database tas nasa lesson2.htnl yung exp points

---
+++ folder/dbh.inc.php
@@ -0,0 +1,14 @@
+<?php
+
+// ********************** pagconnect ng DB*******************//
+
+$dsn = "mysql:host=localhost;dbname=caieldb";
+$dbusername = "root";
+$dbpassword = "";
+
+try {
+    $pdo = new PDO($dsn, $dbusername, $dbpassword);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
